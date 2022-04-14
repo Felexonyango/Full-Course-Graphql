1bb5dbc7418becc11cbaf326e05d75c3a2229d7c
Felexonyango <felexonyango19@gmail.com>
2022-04-14 23:28:17 +0300

    v1

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css'
+import CharacterList from './pages/CharacterList';
+function App() {
+
+  
+  return (
+    <div className="App">
+   Hello apollo
+   <CharacterList/>
+    </div>
+  );
+}
+
+export default App;
